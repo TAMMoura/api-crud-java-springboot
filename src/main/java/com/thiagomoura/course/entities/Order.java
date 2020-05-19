ed90ed48a5e0d86f2e6e89617bd5763b05bf4cd7
Thiago Moura <thiago_moura@hotmail.com.br>
2020-05-19 19:35:36 -0300

    Order, Instant, ISO 8601 - part 1

---
+++ src/main/java/com/thiagomoura/course/entities/Order.java
@@ -0,0 +1,88 @@
+package com.thiagomoura.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant momento;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	public Order() {
+		
+	}
+
+	public Order(Long id, Instant momento, User client) {
+		super();
+		this.id = id;
+		this.momento = momento;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Instant momento) {
+		this.momento = momento;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setCliente(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
